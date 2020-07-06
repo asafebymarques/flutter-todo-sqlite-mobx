b24d7e7f7bd9f6f3da50c2d4ab1815b30047b096
Asafe Marques <asafebymarques@icloud.com>
2020-07-05 22:41:30 -0300

    Ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app_sqlite_mobx/pages/login_page.dart';
+import 'modules/login/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
